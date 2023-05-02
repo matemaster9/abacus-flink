21d6caff70d98326affb63590c8b82a3b580c3ab
cyborg <matemaster@163.com>
2023-05-02 14:41:13 +0800

    abacus-flink init

---
+++ flink-java/src/main/java/org/mastercs/bigdata/flink_java/WordCountCase.java
@@ -0,0 +1,15 @@
+package org.mastercs.bigdata.flink_java;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Author: cyborg
+ * Create: 2023/5/2 14:08
+ */
+public class WordCountCase {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    }
+}
---
+++ flink-scala/src/main/scala/WordCount.scala
@@ -0,0 +1,10 @@
+/**
+ * Author: cyborg
+ * Create: 2023/5/2 14:39
+ */
+object WordCount {
+
+    def main(args: Array[String]): Unit = {
+
+    }
+}
