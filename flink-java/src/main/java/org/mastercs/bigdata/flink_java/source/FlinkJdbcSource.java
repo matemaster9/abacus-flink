16d921436ccbfb8ef20fe40e0833de2f55197c12
syler <matemaster@163.com>
2023-06-27 20:31:56 +0800

    jdbc source(待实现)

---
+++ flink-java/src/main/java/org/mastercs/bigdata/flink_java/source/FlinkJdbcSource.java
@@ -0,0 +1,12 @@
+package org.mastercs.bigdata.flink_java.source;
+
+/**
+ * author: Syler
+ * time: 2023/6/27 19:43
+ */
+public class FlinkJdbcSource {
+
+    public static void main(String[] args) {
+
+    }
+}
